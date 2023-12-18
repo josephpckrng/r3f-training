30949dabeac2130a5f0610998bb2205574f641bb
josephpckrng <ammor139@icloud.com>
2023-12-18 19:10:29 +0000

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,67 @@
-import logo from './logo.svg';
+import { useState } from 'react'
+import { Canvas } from '@react-three/fiber'
+import {
+  useGLTF,
+  OrbitControls,
+  Environment,
+  Stats,
+  Html,
+} from '@react-three/drei'
+import { useControls } from 'leva'
+import Models from './models.json'
 import './App.css';
+function Model({ url }) {
+  const { scene } = useGLTF(url)
+  const [cache, setCache] = useState({})
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  if (!cache[url]) {
+    const annotations = []
+
+    scene.traverse((o) => {
+      if (o.userData.prop) {
+        annotations.push(
+          <Html
+            key={o.uuid}
+            position={[o.position.x, o.position.y, o.position.z]}
+            distanceFactor={0.25}
+          >
+            <div className="annotation">{o.userData.prop}</div>
+          </Html>
+        )
+      }
+    })
+
+    console.log('Caching JSX for url ' + url)
+    setCache({
+      ...cache,
+      [url]: <primitive object={scene}>{annotations}</primitive>,
+    })
+  }
+  return cache[url]
 }
 
-export default App;
+export default function App() {
+  const { model } = useControls({
+    model: {
+      value: 'hammer',
+      options: Object.keys(Models),
+    },
+  })
+
+  return (
+    <>
+      <Canvas camera={{ position: [0, 0, -0.2], near: 0.025 }}>
+        <Environment files="/img/workshop_1k.hdr" background />
+        <group>
+          <Model url={Models[model]} />
+        </group>
+        <OrbitControls autoRotate />
+        <Stats />
+      </Canvas>
+      <span id="info">
+        The {model.replace(/([A-Z])/g, ' $1').toLowerCase()} is selected.
+      </span>
+    </>
+  )
+}
+
